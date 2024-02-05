0224d43687ab4b239199ff4c54079eb37831e120
omkeshborse <bomkesh25@gmail.com>
2024-02-05 10:39:27 +0530

    feat : fetch data add in html remove hardcode value
    data fetch with asych await and then render in index.html by method forEach Inside Cards component in  index.html document

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const cards = document.querySelector(".cards");
+console.log(cards);
+
 const url =
   "https://newsapi.org/v2/everything?q=tesla&from=2024-01-05&sortBy=publishedAt&apiKey=f1c80de658354e51bd7853ce2788e274";
 
@@ -12,5 +15,38 @@ async function dataRequest() {
   }
 }
 const temp = dataRequest(url).then((data) => {
-  data.articles.forEach((item) => {});
+  data.articles.forEach((item) => {
+    const { title, description, source, url, urlToImage, publishedAt } = item;
+    cards.innerHTML += `
+                          <div class="card">
+                            <div class="image">
+                                <img src="${urlToImage}" alt="${source.name}">
+                            </div>
+                            <div class="information">
+                                <div>
+                                    <p class="title">${title}</p>
+                                    <p class="description">${description}</p>
+                                    <p class="time">
+                                        <span>${
+                                          publishedAt
+                                            .replace("Z", " ")
+                                            .split("T")[1]
+                                        }</span>
+                                        <span>${
+                                          publishedAt
+                                            .replace("Z", " ")
+                                            .split("T")[0]
+                                        }</span>
+                                    </p>
+                                </div>
+                                <div class="other">
+                                    <span class="source">${source.name}</span>
+                                    <a class="url" href="${url}" target="_blank">Read Article <i class="fa-solid fa-arrow-right-long"></i></a>
+                                </div>
+                            </div>
+                        </div>
+    
+    
+    `;
+  });
 });
